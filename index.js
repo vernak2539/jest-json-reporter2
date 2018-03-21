46f77af3917ab7f3c46c2449509dd6f7ecd69e7c
Alex Vernacchia <avernacchia@salesforce.com>
2018-03-21 10:30:25 +0000

    use mkdirp sync method

--- index.js
+++ index.js
@@ -9,27 +9,23 @@ module.exports = class JsonReporter {
   onRunComplete (contexts, results) {
     const { outputDir = './coverage', outputFile = 'test-results.json', fullOutput = false } = this._options;
 
-    mkdirp(outputDir, err => {
-      if (err) {
-        throw err;
+    mkdirp.sync(outputDir);
+
+    const generalData = {
+      tests: {
+        failed: results.numFailedTests,
+        skipped: results.numPendingTests,
+        success: results.numPassedTests
+      },
+      suites: {
+        failed: results.numFailedTestSuites,
+        skipped: results.numPendingTestSuites,
+        success: results.numPassedTestSuites
       }
+    };
 
-      const generalData = {
-        tests: {
-          failed: results.numFailedTests,
-          skipped: results.numPendingTests,
-          success: results.numPassedTests
-        },
-        suites: {
-          failed: results.numFailedTestSuites,
-          skipped: results.numPendingTestSuites,
-          success: results.numPassedTestSuites
-        }
-      };
+    const dataToWrite = fullOutput ? results : generalData;
 
-      const dataToWrite = fullOutput ? results : generalData;
-
-      fs.writeFileSync(`${outputDir}/${outputFile}`, JSON.stringify(dataToWrite), 'utf-8');
-    });
+    fs.writeFileSync(`${outputDir}/${outputFile}`, JSON.stringify(dataToWrite), 'utf-8');
   }
 };
